e9d9ac58cc8a05a418d5d481e9856bcea45143c6
khsdev22 <khsdev22@gmail.com>
2022-06-30 03:46:56 +0900

    variable

---
+++ chap02/src/sec01/exam01/VariableInitializationExample.java
@@ -0,0 +1,10 @@
+package sec01.exam01;
+
+public class VariableInitializationExample {
+	public static void main(String[] args) {
+		// 변수 value 선언
+		int value = 3;
+		int result = value + 10;
+		System.out.println(result);
+	}
+}
