e0a8548f4e55c2e0240eb71d9289219faae9e682
khsdev22 <khsdev22@gmail.com>
2022-06-29 23:09:09 +0900

    first commit

---
+++ chap01/src/sec03/exam01/Hello.java
@@ -0,0 +1,16 @@
+package sec03.exam01;
+
+/*
+ 작성자 : 김현수
+ 작성일 : 2022-06-29
+ */
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Java");
+
+	}
+
+}
