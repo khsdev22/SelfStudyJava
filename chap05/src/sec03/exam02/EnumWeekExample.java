999ac7658d6ae0314bafef9545565805ece2c6de
khsdev22 <khsdev22@gmail.com>
2022-07-05 09:10:51 +0900

    enum

---
+++ chap05/src/sec03/exam02/EnumWeekExample.java
@@ -0,0 +1,25 @@
+//package sec03.exam02;
+//
+//import java.util.Calendar;
+//
+//import sec03.exam01.Week;
+//
+//public class EnumWeekExample {
+//	public static void main(String[] args) {
+//		
+//		Week today = null;
+//		
+//		Calendar cal = Calendar.getInstance();
+//		int Week = cal.get(Calendar.DAY_OF_WEEK);
+//		
+//		switch (Week) {
+//			case 1:
+//				today = 
+//				break;
+//			case 2:
+//				today = Week.MONDAY
+//				break;
+//		}
+//		
+//	}
+//}
