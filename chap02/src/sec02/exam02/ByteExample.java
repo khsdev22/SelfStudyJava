b2d758e29ba7aae2f321ac948e6e0fe3db6aaa33
khsdev22 <khsdev22@gmail.com>
2022-07-06 16:31:31 +0900

    객체지향

--- chap02/src/sec02/exam02/ByteExample.java
+++ chap02/src/sec02/exam02/ByteExample.java
@@ -1,20 +1,20 @@
-package sec02.exam02;
-
-public class ByteExample {
-	public static void main(String[] args) {
-		
-		byte var1 = -120;
-		byte var2 = -30;
-		byte var3 = 0;
-		byte var4 = 127;
-		byte var5 = 128;
-		
-		System.out.println(var1);
-		System.out.println(var2);
-		System.out.println(var3);
-		System.out.println(var4);
-		System.out.println(var5);
-		
-		
-	}
-}
+//package sec02.exam02;
+//
+//public class ByteExample {
+//	public static void main(String[] args) {
+//		
+//		byte var1 = -120;
+//		byte var2 = -30;
+//		byte var3 = 0;
+//		byte var4 = 127;
+//		byte var5 = 128;
+//		
+//		System.out.println(var1);
+//		System.out.println(var2);
+//		System.out.println(var3);
+//		System.out.println(var4);
+//		System.out.println(var5);
+//		
+//		
+//	}
+//}
---
+++ chap06/src/sec03/exam02/KoreanExample.java
@@ -0,0 +1,15 @@
+package sec03.exam02;
+
+public class KoreanExample {
+	public static void main(String args) {
+		Korean k1 = new Korean("김현수","910630-1");
+		Korean k2 = new Korean("송지현","891004-2");
+		System.out.println("k1.name : " + k1.name);
+		System.out.println("k1.ssn : " + k1.ssn);
+		System.out.println("k2.name : " + k2.name);
+		System.out.println("k2.ssn : " + k2.ssn);
+		
+	}
+
+}
+  
