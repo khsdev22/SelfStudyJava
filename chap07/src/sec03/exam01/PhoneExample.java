df0dac173a6c1716e8b6b95e556cce9319ee3dd0
khsdev22 <khsdev22@gmail.com>
2022-07-17 04:47:13 +0900

    ch08

---
+++ chap07/src/sec03/exam01/PhoneExample.java
@@ -0,0 +1,13 @@
+package sec03.exam01;
+
+public class PhoneExample {
+	public static void main(String[] args) {
+		
+		SmartPhone smartPhone = new SmartPhone("±èÇö¼ö");
+		
+		smartPhone.TurnOn();
+		smartPhone.internetSearch();
+		smartPhone.TurnOff();
+		
+	}
+}
