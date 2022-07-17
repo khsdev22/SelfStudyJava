51ad7ef7353e05f39fba8859bcee82a309875b8b
khsdev22 <khsdev22@gmail.com>
2022-07-17 12:16:41 +0900

    다형성

---
+++ chap08/src/chap08/Example.java
@@ -0,0 +1,19 @@
+//package chap08;
+//
+//public class Example {
+//	public static void main(String args) {
+//		ImplementationC impl = new ImplementationC();
+//		
+//		InterfaceA ia = impl;
+//		ia.MethodA();
+//		
+//		InterfaceB ib = impl;
+//		ib.MethodB();
+//		
+//		InterfaceC ic = impl;
+//		
+//		ic.MethodA();
+//		ic.MethodB();
+//		ic.methodC();
+//	}
+//}
---
+++ chap08/src/chap08/ImplementationC.java
@@ -0,0 +1,16 @@
+package chap08;
+
+public class ImplementationC {
+	public void methodA() {
+		System.out.println("A실행");
+	}
+	
+	public void methodB() {
+		System.out.println("B실행");
+	}
+	
+	public void methodC() {
+		System.out.println("C실행");
+	}
+
+}
